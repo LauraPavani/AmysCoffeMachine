74ba16760a68445cd2814813d7e659763e46093d
laura <laura.carolpavani@gmail.com>
2025-04-03 22:50:38 +1100

    This exercise should have enhanced your understanding of how to use parameters in Kotlin functions, especially when dealing with different types and scenarios.

--- app/src/main/java/com/example/amyscoffeemachine/MakeCoffee.kt
+++ app/src/main/java/com/example/amyscoffeemachine/MakeCoffee.kt
@@ -1,17 +1,21 @@
 package com.example.amyscoffeemachine
 
 fun main(){
-    makeCoffee(1)
-    makeCoffee(2)
-    makeCoffee(30)
+    makeCoffee(0, "Eduardo")
+    makeCoffee(1, "Lucelia")
+    makeCoffee(2, "Jimmy")
+    makeCoffee(3, "Lucinda")
+    makeCoffee(5, "Helena")
 
 }
 
-fun makeCoffee(sugarCount: Int){
+fun makeCoffee(sugarCount: Int, name: String){
 
     if (sugarCount == 1){
-        println("Coffee with 1 spoon of sugar")
+        println("Coffee with 1 spoon of sugar for $name")
+    }else if(sugarCount == 0){
+        println("Coffee with no sugar for $name")
     }else{
-        println("Coffee with $sugarCount spoons of sugar")
+        println("Coffee with $sugarCount spoons of sugar for $name")
     }
 }
